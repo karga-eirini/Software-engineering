b2d81402db4fe794005f6633f48bdc6d89949eb3
MariosPouliasis <80156313+MariosPouliasis@users.noreply.github.com>
2021-04-01 13:49:59 +0300

    Update main_menu.py

--- main_menu.py
+++ main_menu.py
@@ -1,27 +1,18 @@
 from tkinter import *
 import os
-
+from PIL import ImageTk,Image
 
 window = Tk()
 window.title('Frienvi Main Menu')
 window.geometry("700x700")
-logo= Canvas(window, width = 285, height = 126)
 window.configure(bg='lightblue')
-Grid.columnconfigure(window,0,weight=1)
-Grid.rowconfigure(window,0,weight=1)
-Grid.rowconfigure(window,1,weight=1)
-Grid.rowconfigure(window,2,weight=1)
-Grid.rowconfigure(window,3,weight=1)
-Grid.rowconfigure(window,4,weight=1)
-Grid.rowconfigure(window,5,weight=1)
-Grid.rowconfigure(window,5,weight=1)
-Grid.rowconfigure(window,6,weight=1)
-Grid.rowconfigure(window,7,weight=1)
 
 
 #for logo
+logo = Canvas(window, width = 285, height = 126)
 img = PhotoImage(file = 'C:\\Users\\mario\\Desktop\\Μαθήματα\\8ο εξάμηνο\\Τεχνολογία Λογισμικού\\εικόνες\\logo.png')
 logo.create_image(10,10,anchor=NW, image=img)
+logo.grid(row=0,column=0)
 
 #menubar
 menu=Menu(window)
@@ -46,31 +37,32 @@ subm4.add_command(label="Πρόταση καταστήματος")
 
 
 
-
+#buttons
 search = Entry(window,width=30,borderwidth=1)
 search.insert(0, "Αναζήτηση")
+search.grid(row=1,column=0)
 
 news=Button(window, text="Ειδήσεις",height=3,width=30)
-news.grid(row=3,column=0)
+news.grid(row=2,column=0)
 
-clothes=Button(window, text="Μεταχειρισμένα",font=('bold'),heigh=3,width=30)
-clothes.grid(row=4,column=0)
+secondh=Button(window, text="Μεταχειρισμένα",font=('bold'),heigh=3,width=30)
+secondh.grid(row=3,column=0)
 
-obj=Button(window, text="Χώροι εστίασης",font=('bold'),heigh=3,width=30)
-obj.grid(row=5,column=0)
+estiash=Button(window, text="Χώροι εστίασης",font=('bold'),heigh=3,width=30)
+estiash.grid(row=4,column=0)
 
-estiash=Button(window, text="Καταστήματα τροφίμων",font=('bold'),heigh=3,width=30)
-estiash.grid(row=6,column=0)
+food=Button(window, text="Καταστήματα τροφίμων",font=('bold'),heigh=3,width=30)
+food.grid(row=5,column=0)
 
+#list of buttons
+obj_list=[logo,search,news,secondh,estiash,food]
+#loop thru the list and config
+row_num=0
+for button in obj_list:
+    Grid.rowconfigure(window,row_num,weight=1)
+    row_num+=1
 
-
-
-#placement
-logo.grid(row=0 ,column=0)
-search.grid(row=2,column=0)
-
-
-
+Grid.columnconfigure(window,0,weight=1)
 
 
 
