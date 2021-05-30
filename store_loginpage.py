144c82813f0dc8188237f6bd630db6bfa506a19d
karga-eirini <80267831+kargaa-eirini@users.noreply.github.com>
2021-05-30 15:59:09 +0300

    Update store_loginpage.py

--- store_loginpage.py
+++ store_loginpage.py
@@ -1,96 +1,190 @@
 from tkinter import *
 import os
+import tkinter as tk 
 from PIL import ImageTk,Image
 
-global logo
-store_login=Tk()
-store_login.title('Frienvi Store login page')
-store_login.geometry("500x500")
-#app logo
-logo= Canvas(store_login, width = 190, height = 91)
-img = ImageTk.PhotoImage(Image.open('frienvilogo.png'))
-logo.create_image(10,10,anchor=NW, image=img)
-logo.theimage=img
 
-#entry request screen
-def entryRequest():
-    global entryRequest_screen
-    entryRequest_screen=Toplevel(store_login)
-    entryRequest_screen.title("Frienvi Store Entry Request")
-    entryRequest_screen.geometry("800x800")
+# main store log in page
+class Store_login(tk.Frame):
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        
+        #app logo
+        logo= Canvas(self, width = 190, height = 91)
+        img = ImageTk.PhotoImage(Image.open('frienvilogo.png'))
+        logo.create_image(10,10,anchor=NW, image=img)
+        logo.theimage=img
 
-    logo2=Canvas(entryRequest_screen, width = 190, height = 91)
-    img2=ImageTk.PhotoImage(Image.open('frienvilogo.png'))
-    logo2.create_image(10,10,anchor=NW, image=img)
-    global name
-    global storeinfo
-    global ecoinfo
-    global answer
-    name=StringVar()
-    storeinfo=StringVar()
-    ecoinfo=StringVar()
-    answer=StringVar()
-    
-    name=Entry(entryRequest_screen,textvariable=name)
-    application=Label(entryRequest_screen, text="Αίτηση εισαγωγής",font=('ariel', 10))
-    storename=Label(entryRequest_screen, text="Όνομα Καταστήματος",font=('ariel', 10))
-    enterdetails=Label(entryRequest_screen, text="Πείτε μας λίγα λόγια για την επιχειρησή σας",font=('ariel', 10))
-    storeinfo=Entry(entryRequest_screen,textvariable=storeinfo)
-    ecodetails=Label(entryRequest_screen, text="Ενημερώστε μας για την επίπτωση που έχει το κατάστημά σας στο περιβάλλον",font=('ariel', 10))
-    ecoinfo=Entry(entryRequest_screen,textvariable=ecoinfo)
-    question=Label(entryRequest_screen, text="Γιατί πιστεύεται πρέπει να σας προσθέσουμε στην εφαρμογή;",font=('ariel', 10))
-    answer=Entry(entryRequest_screen,textvariable=answer)
-    confirm=Button(entryRequest_screen,text='Επιβεβαίωση',width=20, height=1,bg='black',fg='white')
-    goback=Button(entryRequest_screen,text='Επιστροφή',width=20, height=1,bg='black',fg='white' )
+        label_1=Label(self, text="Έχετε επιβεβαιώσει την εισαγωγή σας ως κατάστημα;",font=('ariel', 10))
 
-    logo2.grid(row=0,column=0)
-    application.grid(row=1,column=0)
-    storename.grid(row=2,column=0)
-    name.grid(row=2,column=1)
-    enterdetails.grid(row=3,column=0)
-    storeinfo.grid(row=3,column=1,ipadx=30,ipady=30)
-    ecodetails.grid(row=4,column=0)
-    ecoinfo.grid(row=4,column=1,ipadx=30,ipady=30)
-    question.grid(row=5,column=0)
-    answer.grid(row=5,column=1,ipadx=30,ipady=30)
-    confirm.grid(row=6,column=1)
-    goback.grid(row=6,column=2)
+        button_1=tk.Button(self, text='Αίτηση εισαγωγής',width=40, height=2,bg='black',fg='white', command=lambda: controller.show_frame(EntryRequest))
+        button_2=tk.Button(self, text='Δημιουργία Προφίλ Καταστήματος',width=40, height=2,bg='black',fg='white')
 
-    #list of buttons
-    obj_list=[logo2,application,storename,name,enterdetails,storeinfo,ecodetails,ecoinfo,question,answer,confirm,goback]
-    #loop thru the list and config
-    row_num=0
-    for button in obj_list:
-        Grid.rowconfigure(entryRequest_screen,row_num,weight=1)
-        row_num+=1
+        logo.grid(row=0,column=0)
+        label_1.grid(row=1,column=0)
+        button_1.grid(row=2,column=0)
+        button_2.grid(row=3,column=0)
 
-    Grid.columnconfigure(entryRequest_screen,0,weight=1)
+        #list of buttons
+        obj_list=[logo,label_1,button_1,button_2]
+        #loop thru the list and config
+        row_num=0
+        for button in obj_list:
+            Grid.rowconfigure(self,row_num,weight=1)
+            row_num+=1
+
+        Grid.columnconfigure(self,0,weight=1)
+
+        
+
+#entry request page
+class EntryRequest(tk.Frame):
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        
+        
+        logo2=Canvas(self, width = 190, height = 91)
+        img2=ImageTk.PhotoImage(Image.open('frienvilogo.png'))
+        logo2.create_image(10,10,anchor=NW, image=img2)
+        global storelist
+        global adeia
+        global ecoinfo
+        global storedetails
+        storelist=StringVar()
+        adeia=StringVar()
+        ecoinfo=StringVar()
+        storedetails=StringVar()
+
+        #labels
+        label_1=Label(self, text="Αίτηση εισαγωγής",font=('ariel', 20,'bold'))
+        label_2=Label(self, text="Όνομα Καταστήματος",font=('ariel', 10))
+        label_3=Label(self, text="Πείτε μας λίγα λόγια για την επιχειρησή σας",font=('ariel', 10))
+        label_4=Label(self, text="Ενημερώστε μας για την επίπτωση που έχει το κατάστημά σας στο περιβάλλον",font=('ariel', 10))
+        label_5=Label(self, text="Γιατί πιστεύεται πρέπει να σας προσθέσουμε στην εφαρμογή;",font=('ariel', 10))
+
+        #entry boxes
+        storelist=Entry(self,textvariable=storelist)
+        adeia=Entry(self,textvariable=adeia)
+        ecoinfo=Entry(self,textvariable=ecoinfo)
+        storedetails=Entry(self,textvariable=storedetails)
+
+        #buttons
+        confirm=tk.Button(self,text='Επιβεβαίωση',width=20, height=1,bg='black',fg='white',command=lambda: controller.show_frame(DocumentsList))
+        goback=tk.Button(self,text='Επιστροφή',width=20, height=1,bg='black',fg='white',command=lambda: controller.show_frame(Store_login))
+
+        #the entry request screen
+        logo2.grid(row=0,column=0)
+        label_1.grid(row=1,column=0)
+        label_2.grid(row=2,column=0)
+        storelist.grid(row=2,column=1)
+        label_3.grid(row=3,column=0)
+        adeia.grid(row=3,column=1,ipadx=30,ipady=30)
+        label_4.grid(row=4,column=0)
+        ecoinfo.grid(row=4,column=1,ipadx=30,ipady=30)
+        label_5.grid(row=5,column=0)
+        storedetails.grid(row=5,column=1,ipadx=30,ipady=30)
+        confirm.grid(row=6,column=1)
+        goback.grid(row=6,column=2)
+
+        #list of buttons
+        obj_list=[logo2,label_1,label_2,storelist,label_3,adeia,label_4,ecoinfo,label_5,storedetails,confirm,goback]
+        #loop thru the list and config
+        row_num=0
+        for button in obj_list:
+            Grid.rowconfigure(self,row_num,weight=1)
+            row_num+=1
+
+        Grid.columnconfigure(self,0,weight=1)
+
+        
+
+#document list page
+class DocumentsList(tk.Frame):
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        
+
+        logo3=Canvas(self, width = 190, height = 91)
+        img3=ImageTk.PhotoImage(Image.open('frienvilogo.png'))
+        logo3.create_image(10,10,anchor=NW, image=img3)
+        global storelist
+        global adeia
+        global ecoinfo
+        global storedetails
+        storelist=StringVar()
+        adeia=StringVar()
+        ecoinfo=StringVar()
+        storedetails=StringVar()
+
+        #labels
+        label_1=Label(self, text="Λίστα δικαιολογητικών",font=('ariel', 20,'bold'))
+        label_2=Label(self, text="Άδεια λειτουργίας καταστήματος",font=('ariel', 10))
+        label_3=Label(self, text="Έγγραφο οικολογικού αντίκτυπου",font=('ariel', 10))
+        label_4=Label(self, text="Πληροφορίες επικοινωνίας",font=('ariel', 10))
+        label_5=Label(self, text="Πληροφορίες καταστήματος",font=('ariel', 10))
+
+        #entry boxes
+        storelist=Entry(self,textvariable=storelist)
+        adeia=Entry(self,textvariable=adeia)
+        ecoinfo=Entry(self,textvariable=ecoinfo)
+        storedetails=Entry(self,textvariable=storedetails)
+
+        #buttons
+        confirm=tk.Button(self,text='Επιβεβαίωση',width=20, height=1,bg='black',fg='white')
+        goback=tk.Button(self,text='Επιστροφή',width=20, height=1,bg='black',fg='white',command=lambda: controller.show_frame(EntryRequest))
+
+        #the document list screen
+        logo3.grid(row=0,column=0)
+        label_1.grid(row=1,column=0)
+        label_2.grid(row=2,column=0)
+        storelist.grid(row=2,column=1)
+        label_3.grid(row=3,column=0)
+        adeia.grid(row=3,column=1,ipadx=30,ipady=30)
+        label_4.grid(row=4,column=0)
+        ecoinfo.grid(row=4,column=1,ipadx=30,ipady=30)
+        label_5.grid(row=5,column=0)
+        storedetails.grid(row=5,column=1,ipadx=30,ipady=30)
+        confirm.grid(row=6,column=1)
+        goback.grid(row=6,column=2)
+
+        #list of buttons
+        obj_list=[logo3,label_1,label_2,storelist,label_3,adeia,label_4,ecoinfo,label_5,storedetails,confirm,goback]
+        #loop thru the list and config
+        row_num=0
+        for button in obj_list:
+            Grid.rowconfigure(self,row_num,weight=1)
+            row_num+=1
+
+        Grid.columnconfigure(self,0,weight=1)
 
 
 
-    
-    
+class Application(tk.Tk):
+    def __init__(self, *args, **kwargs):
+        tk.Tk.__init__(self, *args, **kwargs)
+
+        # creating a window
+        window = tk.Frame(self)
+        window.pack()
+
+        window.grid_rowconfigure(0, minsize=500)
+        window.grid_columnconfigure(0, minsize=800)
+
+        self.frames = {}
+        for F in (Store_login, EntryRequest,DocumentsList):
+            frame = F(window, self)
+            self.frames[F] = frame
+            frame.grid(row=0, column=0, sticky="nsew")
+
+        self.show_frame(Store_login)
+
+    def show_frame(self, page):
+        frame = self.frames[page]
+        frame.tkraise()
+        self.title("Store page")
+        
 
 
-
-
-label_1=Label(store_login, text="Έχετε επιβεβαιώσει την εισαγωγή σας ως κατάστημα;",font=('ariel', 10))
-button_1=Button(store_login, text='Αίτηση εισαγωγής',width=40, height=2,bg='black',fg='white', command=entryRequest)
-button_2=Button(store_login, text='Δημιουργία Προφίλ Καταστήματος',width=40, height=2,bg='black',fg='white')
-
-logo.grid(row=0,column=0)
-label_1.grid(row=1,column=0)
-button_1.grid(row=2,column=0)
-button_2.grid(row=3,column=0)
-
-#list of buttons
-obj_list=[logo,label_1,button_1,button_2]
-#loop thru the list and config
-row_num=0
-for button in obj_list:
-    Grid.rowconfigure(store_login,row_num,weight=1)
-    row_num+=1
-
-Grid.columnconfigure(store_login,0,weight=1)
-
-store_login.mainloop()
+app = Application()
+app.maxsize(800, 500)
+app.mainloop()
