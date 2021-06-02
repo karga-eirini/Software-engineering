513206b3d28c2a0e10130f73c26c1b37ba6fd20d
karga-eirini <80267831+kargaa-eirini@users.noreply.github.com>
2021-06-02 17:58:22 +0300

    Update store_loginpage.py

--- store_loginpage.py
+++ store_loginpage.py
@@ -44,8 +44,7 @@ class Store_login(tk.Frame):
 
         Grid.columnconfigure(self,0,weight=1)
 
-        
-
+       
 #entry request page
 class EntryRequest(tk.Frame):
     def __init__(self, parent, controller):
@@ -58,14 +57,14 @@ class EntryRequest(tk.Frame):
         logo = Label(self, image=render)
         logo.image = render
 
-        global storelist
-        global adeia
-        global ecoinfo
-        global storedetails
-        storelist=StringVar()
-        adeia=StringVar()
-        ecoinfo=StringVar()
-        storedetails=StringVar()
+        global storename
+        global details
+        global eco
+        global info
+        storename=StringVar()
+        details=StringVar()
+        eco=StringVar()
+        info=StringVar()
 
         #labels
         label_1=Label(self, text="Αίτηση εισαγωγής",font=('ariel', 20,'bold'))
@@ -75,10 +74,10 @@ class EntryRequest(tk.Frame):
         label_5=Label(self, text="Γιατί πιστεύεται πρέπει να σας προσθέσουμε στην εφαρμογή;",font=('ariel', 10))
 
         #entry boxes
-        storelist=Entry(self,textvariable=storelist)
-        adeia=Entry(self,textvariable=adeia)
-        ecoinfo=Entry(self,textvariable=ecoinfo)
-        storedetails=Entry(self,textvariable=storedetails)
+        storename=Entry(self,textvariable=storename)
+        details=Entry(self,textvariable=details)
+        eco=Entry(self,textvariable=eco)
+        info=Entry(self,textvariable=info)
 
         #buttons
         confirm=tk.Button(self,text='Επιβεβαίωση',width=20, height=1,bg='black',fg='white',command=lambda: controller.show_frame(DocumentsList))
@@ -88,18 +87,18 @@ class EntryRequest(tk.Frame):
         logo.grid(row=0,column=0)
         label_1.grid(row=1,column=0)
         label_2.grid(row=2,column=0)
-        storelist.grid(row=2,column=1)
+        storename.grid(row=2,column=1)
         label_3.grid(row=3,column=0)
-        adeia.grid(row=3,column=1,ipadx=30,ipady=30)
+        details.grid(row=3,column=1,ipadx=30,ipady=30)
         label_4.grid(row=4,column=0)
-        ecoinfo.grid(row=4,column=1,ipadx=30,ipady=30)
+        eco.grid(row=4,column=1,ipadx=30,ipady=30)
         label_5.grid(row=5,column=0)
-        storedetails.grid(row=5,column=1,ipadx=30,ipady=30)
+        info.grid(row=5,column=1,ipadx=30,ipady=30)
         confirm.grid(row=6,column=1)
         goback.grid(row=6,column=2)
 
         #list of buttons
-        obj_list=[logo,label_1,label_2,storelist,label_3,adeia,label_4,ecoinfo,label_5,storedetails,confirm,goback]
+        obj_list=[logo,label_1,label_2,storename,label_3,details,label_4,eco,label_5,info,confirm,goback]
         #loop thru the list and config
         row_num=0
         for button in obj_list:
