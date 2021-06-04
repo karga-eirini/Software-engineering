287d97ec959c37d3cbe411301b2b359c8813a0f7
xristina.sykara <65867825+XristinaSyk@users.noreply.github.com>
2021-06-04 17:30:07 +0300

    Update user_comment.py

--- user_comment.py
+++ user_comment.py
@@ -1,51 +1,41 @@
 from tkinter import *
 import os
 from PIL import ImageTk,Image
+from tkinter import ttk
+import tkinter as tk
+from tkinter import messagebox
 
-global username
+def confirm_message():
+    MsgBox=tk.messagebox.showinfo("Confirm","Επιτυχία υποβολής σχολίου!") 
+    if MsgBox == 'ok':
+        sys.exit()
 
-platforma= Tk()
-platforma.title('Frienvi')
-platforma.geometry("450x450")
+platform= Tk()
+platform.title('Frienvi')
+platform.geometry("450x450")
+
 
 #app logo
-logo= Canvas(platforma, width = 290, height = 120)
+logo= Canvas(platform, width = 290, height = 120)
 img = ImageTk.PhotoImage(Image.open('frienvilogo.png'))
 logo.create_image(10,10,anchor=NW, image=img)
 
 #sxolio
-platforma_screen= Label(platforma,text='Πλατφόρμα επικοινωνίας',font=('arial', 15,'bold'))
-message=Label(platforma,text='Προσθέστε κάποιο σχόλιο',font=('calibri', 10),fg='grey')
-usertext = Label(platforma, text='Σχόλιο')
-user = Text(platforma,width="50", height="5")
+platform_screen= Label(platform,text='Πλατφόρμα επικοινωνίας',font=('arial', 15,'bold'))
+message=Label(platform,text='Προσθέστε κάποιο σχόλιο',font=('calibri', 10),fg='grey')
+usertext = Label(platform, text='Σχόλιο')
+user = Text(platform,width="50", height="5")
 
 
-check=Checkbutton(platforma,text='Είστε σίγουρος;')
-
-def epityxia():
-    global epityxia_screen
-    epityxia_screen = Toplevel(platforma_screen)
-    epityxia_screen.title("Frienvi")
-    epityxia_screen.geometry("450x450")
-
-    #second screen
-    logo2 = Canvas(epityxia_screen, width = 290, height = 120)
-    img2 = ImageTk.PhotoImage(Image.open('frienvilogo.png'))
-    logo2.create_image(5,5,anchor=NW, image=img)
-
-    logo2.pack(pady=(0,20))
-    Label(epityxia_screen, text="Επιτυχία υποβολής!").pack(pady=(0,20))
-    Label(epityxia_screen, text="Επιτυχής αποθήκευση σχολίου!").pack(pady=(0,20))
-
-
+check=Checkbutton(platform,text='Είστε σίγουρος;')
 
 logo.pack(pady=(0,20 ))
-platforma_screen.pack(pady=(0,20))
+platform_screen.pack(pady=(0,20))
 message.pack()
 usertext.pack()
 user.pack(pady=(0,20))
 check.pack(pady=(20,0))
-Button(text='Υποβολή',width=16, height=1,bg='green',fg='white',command=epityxia).pack()
+Button(text='Υποβολή',width=16, height=1,bg='green',fg='white',command=confirm_message).pack()
 
 
-platforma.mainloop()
+platform.mainloop()
