748cdf2d128ac9691bc1d69f8128ee833bcd6b11
xristina.sykara <65867825+XristinaSyk@users.noreply.github.com>
2021-06-05 15:57:30 +0300

    Update main_menu.py

--- main_menu.py
+++ main_menu.py
@@ -9,7 +9,7 @@ import sys
 self = Tk()
 self.title('Frienvi Main Menu')
 self.geometry("500x500")
-self.configure(bg='lightblue')
+self.configure(bg='lightgreen')
 
 
 #app logo
@@ -25,7 +25,7 @@ search = Entry(self,width=30,borderwidth=1)
 search.insert(0, "Αναζήτηση")
 search.grid(row=1,column=0)
 
-news=Button(self, text="Ειδήσεις",height=3,width=30)
+news=Button(self, text="Ειδήσεις",font=('bold'),height=3,width=30)
 news.grid(row=2,column=0)
 
 secondh=Button(self, text="Μεταχειρισμένα",font=('bold'),heigh=3,width=30)
@@ -37,8 +37,11 @@ estiash.grid(row=4,column=0)
 food=Button(self, text="Καταστήματα τροφίμων",font=('bold'),heigh=3,width=30)
 food.grid(row=5,column=0)
 
+platform=Button(self, text="Πλατφόρμα επικοινωνίας",font=('bold'),heigh=3,width=30)
+platform.grid(row=6,column=0)
+
 #list of buttons
-obj_list=[logo,search,news,secondh,estiash,food]
+obj_list=[logo,search,news,secondh,estiash,food,platform]
 #loop thru the list and config
 row_num=0
 for button in obj_list:
@@ -50,4 +53,3 @@ Grid.columnconfigure(self,0,weight=1)
 
 
 self.mainloop()
-
